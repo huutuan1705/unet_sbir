748f1be5abe6173c47a351d30c3ee81402fabf3a
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-04-03 10:30:15 +0700

    update utils

--- dataset/utils.py
+++ dataset/utils.py
@@ -4,8 +4,8 @@ def get_transform(type, size=299):
     if type == 'train':
         transform_list = [
             transforms.Resize(size),
-            transforms.RandomHorizontalFlip(0.5),
-            transforms.RandomRotation(20),
+            # transforms.RandomHorizontalFlip(0.5),
+            # transforms.RandomRotation(20),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]
